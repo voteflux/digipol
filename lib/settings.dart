5163b1a26ba779b040f9598c83ff67af8cbafb16
KipCrossing <kip.crossing@gmail.com>
2020-03-14 19:19:39 +1100

    Make Settings page a bit nicer

--- lib/settings.dart
+++ lib/settings.dart
@@ -21,18 +21,22 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
 
       backgroundColor: appColors.background,
-      body: Container(
-        child: Center(
+      body: Center(
+
+        child: Container(
+          width: appSizes.mediumWidth,
           child: ListView(
             children: <Widget>[
-              Text(
-                "Page Under Construction...",
-                style: TextStyle(fontSize: 30, color: Colors.red),
+              Container(
+                padding: EdgeInsets.all(appSizes.standardPadding),
+                child: Center(
+                  child: Text(
+                  "User Settings",
+                  style: appTextStyles.heading,
+                ),
+                ),
               ),
-              Text(
-                "User Settings",
-                style: TextStyle(fontSize: 30, color: appColors.text),
-              ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -55,21 +59,91 @@ class _SettingsPageState extends State<SettingsPage> {
                   )
                 ],
               ),
-              RaisedButton(
-                elevation: appSizes.cardElevation,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Notifications", style: appTextStyles.standardBold,),
+                  Container(
+                    width: 70,
+                    child: Switch(
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      value: false,
+                      onChanged: (value) {
+                        print("Notifications... ");
+                      },
+                      activeTrackColor: Colors.lightBlue,
+                      activeColor: Colors.blue,
+                    ),
+
+                  )
+                ],
+              ),
+              Card(
                 shape: RoundedRectangleBorder(
-                  side: BorderSide(color: appColors.buttonOutline, width: 2),
-                  borderRadius: new BorderRadius.circular(
-                      appSizes.buttonRadius),
+                    borderRadius: BorderRadius.circular(appSizes.cardCornerRadius)),
+                margin: EdgeInsets.all(appSizes.standardMargin),
+                elevation: appSizes.cardElevation,
+                color: appColors.card,
+                child: Container(
+                  width: appSizes.mediumWidth,
+                  height: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text("User Information", style: appTextStyles.standardBold,),
+                      Text("You need to login to start voting", style: appTextStyles.standardItalic,),
+                      RaisedButton(
+                        elevation: appSizes.cardElevation,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: appColors.buttonOutline, width: 2),
+                          borderRadius: new BorderRadius.circular(
+                              appSizes.buttonRadius),
+                        ),
+                        color: appColors.button,
+                        onPressed: (){
+                          Navigator.of(context).pushNamed(
+                            '/profile',
+                          );
+                        },
+                        child: Text("Login", style: appTextStyles.yesnobutton,),
+                      )
+                    ],
+                  )
                 ),
-                  color: appColors.button,
-                  onPressed: (){
-                    Navigator.of(context).pushNamed(
-                      '/profile',
-                    );
-                  },
-                child: Text("Login", style: appTextStyles.yesnobutton,),
-              )
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(appSizes.cardCornerRadius)),
+                margin: EdgeInsets.all(appSizes.standardMargin),
+                elevation: appSizes.cardElevation,
+                color: appColors.card,
+                child: Container(
+                    width: appSizes.mediumWidth,
+                    height: 150,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text("Voting History", style: appTextStyles.standardBold,),
+                        Text("You need to login to view your voting history", style: appTextStyles.standardItalic,),
+                        RaisedButton(
+                          elevation: appSizes.cardElevation,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: appColors.buttonOutline, width: 2),
+                            borderRadius: new BorderRadius.circular(
+                                appSizes.buttonRadius),
+                          ),
+                          color: appColors.button,
+                          onPressed: (){
+                            Navigator.of(context).pushNamed(
+                              '/profile',
+                            );
+                          },
+                          child: Text("Login", style: appTextStyles.yesnobutton,),
+                        )
+                      ],
+                    )
+                ),
+              ),
             ],
           ),
         ),
