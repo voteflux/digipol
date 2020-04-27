0429f1409c35cd033152e51b64856ad985cf33f0
oscar <oscarmanderj@gmail.com>
2020-04-27 22:23:48 +1000

    add createUser function

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:voting_app/core/models/user.dart';
+import 'package:voting_app/core/services/wallet.dart';
 
 import '../../locator.dart';
 import 'api.dart';
@@ -10,7 +14,32 @@ class AuthenticationService {
 
   StreamController<User> userController = StreamController<User>();
 
+  Future<bool> createUser(String name) async {
+ 
+    final prefs = await SharedPreferences.getInstance();
+
+    //set name
+    prefs.setString('name', name);
+
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+
+    var walletService = WalletService(appDocPath);
+    walletService.make();
+
+    // TO DO: check not to override existing wallet
+    prefs.setString('ethereumAddress', walletService.ethereumAddress().toString());
+
+
+    print(name);
+    return true;
+  }
+
+
+
   Future<bool> login(String name) async {
+
     var fetchedUser = await _api.getUser();
 
     var hasUser = fetchedUser != null;
