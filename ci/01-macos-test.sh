ba640b445df25107f0bf3533ac73591eb20582e8
Max Kaye <m@xk.io>
2020-05-06 05:46:07 +1000

    attempt 1 at github status msgs

--- ci/01-macos-test.sh
+++ ci/01-macos-test.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-flutter test
+mark_status(){
+    _status="${1:-failure}"
+    github-status.py --repo voteflux/voting_app --sha $CI_COMMIT_SHA --status "$_status"
+}
+
+mark_status pending
+
+(flutter test && mark_status success) || \
+    mark_status failure
