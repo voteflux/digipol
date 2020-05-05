01df6d6311626ce2555e5557cf9fb92d3f36c1e8
Max Kaye <m@xk.io>
2020-05-06 06:00:52 +1000

    Update macos tests again to do statuses properly

--- ci/01-macos-test.sh
+++ ci/01-macos-test.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-
-
 mark_status(){
     _status="${1:-failure}"
+    _msg="${2:-No message provided :(}"
     if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
-        github-status.py --repo voteflux/voting_app --sha $CI_COMMIT_SHA --status "$_status"
+        github-status.py --repo voteflux/voting_app --sha $CI_COMMIT_SHA --status "$_status" --desc "$_msg"
+    else
+        echo "[DEBUG] setting github status for voteflux/voting_app (sha: ${CI_COMMIT_SHA}) to $_status: $_msg" 1>&2
     fi
 }
 
-mark_status pending
+mark_status pending "Starting Macos test"
 
-(flutter test && mark_status success) || \
-    mark_status failure
+flutter test 2> >(tee tmp.stderr.txt) | tee tmp.stdout.txt
+
+TEST_RES="${PIPESTATUS[0]}"
+
+if [[ "$TEST_RES" == "0" ]]; then
+    mark_status success "Macos test success"
+else
+    mark_status failure "Failed"
+fi
