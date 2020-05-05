83e8df9a76e263aaa07fcfc37604cd4eb3420e75
Max Kaye <m@xk.io>
2020-05-06 06:54:39 +1000

    update ci scripts and readme

--- ci/01-linux-test.sh
+++ ci/01-linux-test.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-flutter test
+if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
+    STATUS_SCRIPT_URL="https://gist.githubusercontent.com/XertroV/ed38440cb694382852e8dae01c9daed2/raw/ba9b9b326076c375b23365a7cac87b275fea211f/github-status.py"
+    mkdir ~/bin
+    curl "$STATUS_SCRIPT_URL" > ~/bin/github-status
+    chmod +x ~/bin/github-status
+    export PATH="$HOME/bin:$PATH"
+fi
+
+source $(dirname "$0")/00-lib.source.sh
+
+mark_status pending "Starting Android test"
+
+flutter test 2> >(tee tmp.stderr.txt) | tee tmp.stdout.txt
+
+TEST_RES="${PIPESTATUS[0]}"
+
+if [[ "$TEST_RES" == "0" ]]; then
+    mark_status success "Android test success"
+else
+    mark_status failure "Android test failed with status: $TEST_RES"
+fi
+
+exit "$TEST_RES"
--- ci/01-macos-test.sh
+++ ci/01-macos-test.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-mark_status(){
-    _status="${1:-failure}"
-    _msg="${2:-No message provided :(}"
-    if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
-        github-status --repo voteflux/voting_app --sha "$CI_COMMIT_SHA" --status "$_status" --desc "$_msg"
-    else
-        echo "[DEBUG] setting github status for voteflux/voting_app (sha: ${CI_COMMIT_SHA}) to $_status: $_msg" 1>&2
-    fi
-}
+source $(dirname "$0")/00-lib.source.sh
 
-mark_status pending "Starting Macos test"
+mark_status pending "Starting iOS test"
 
 flutter test 2> >(tee tmp.stderr.txt) | tee tmp.stdout.txt
 
 TEST_RES="${PIPESTATUS[0]}"
 
 if [[ "$TEST_RES" == "0" ]]; then
-    mark_status success "Macos test success"
+    mark_status success "iOS test success"
 else
-    mark_status failure "Failed with status; $TEST_RES"
+    mark_status failure "iOS test failed with status: $TEST_RES"
 fi
 
 exit "$TEST_RES"
