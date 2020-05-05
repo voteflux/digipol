ec0f598c32d1c6bf5d684b963d24e5c0b004723f
Max Kaye <m@xk.io>
2020-05-06 07:23:03 +1000

    Tweak gh-status call

--- ci/00-lib.source.sh
+++ ci/00-lib.source.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
-    alias github-status="$(dirname $0)/github-status.py"
-fi
+# if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
+#     alias github-status="$(dirname $0)/github-status.py"
+# fi
 
 mark_status(){
     _status="${1:-failure}"
     _msg="${2:-No message provided :(}"
     if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
-        github-status --repo voteflux/voting_app --sha "$CI_COMMIT_SHA" --status "$_status" --desc "$_msg"
+        "$(dirname $0)/github-status.py" --repo voteflux/voting_app --sha "$CI_COMMIT_SHA" --status "$_status" --desc "$_msg"
     else
         echo "[DEBUG] setting github status for voteflux/voting_app (sha: ${CI_COMMIT_SHA}) to $_status: $_msg" 1>&2
     fi
