99a2195a3c74b1fc0fd62f9a97b2ca6899df5034
Max Kaye <m@xk.io>
2020-05-06 05:48:37 +1000

    Skip github-status.py if we aren't on a CI runner

--- ci/01-macos-test.sh
+++ ci/01-macos-test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+
+
 mark_status(){
     _status="${1:-failure}"
-    github-status.py --repo voteflux/voting_app --sha $CI_COMMIT_SHA --status "$_status"
+    if [[ "$GITLAB_CI" == "true" ]] && [[ "$CI_BUILD_ID" != "" ]]; then
+        github-status.py --repo voteflux/voting_app --sha $CI_COMMIT_SHA --status "$_status"
+    fi
 }
 
 mark_status pending
