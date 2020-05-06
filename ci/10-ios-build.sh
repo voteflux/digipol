d2b56591f9af05c677e1efb44363d16a2cedd9f2
Max Kaye <m@xk.io>
2020-05-06 18:08:32 +1000

    split ios builds and improve ios build script

--- ci/10-ios-build.sh
+++ ci/10-ios-build.sh
@@ -6,14 +6,30 @@ source $_CIDIR/00-lib.source.sh
 
 mark_status "pending" "Beginning iOS build"
 
-do-flux-codesign
+IOS_BUILD_EXTRA=${IOS_BUILD_EXTRA:-""}
+IOS_EXPORT_EXTRA=${IOS_EXPORT_EXTRA:-""}
+export ANDROID_HOME="/Library/Android/sdk"
 
-BUILD_STATUS="$?"
+if [[ "$GITLAB_CI" == "true" ]]; then
+  flutter upgrade
+  flutter clean || true
+  flutter pub cache repair
+fi
 
-if [[ "$BUILD_STATUS" == "0" ]]; then
-    mark_status "success" "iOS build success"
+flutter build ios "$IOS_BUILD_EXTRA"
+FLUTTER_BUILD_STATUS="$?"
+cd ios
+xcodebuild clean archive -workspace Runner.xcworkspace -scheme Runner -archivePath RunnerArchive
+XCODE_CLEAN_ARCHIVE_STATUS="$?"
+xcodebuild -exportArchive -archivePath RunnerArchive.xcarchive -exportOptionsPlist ExportOptions.plist -exportPath ./build "${IOS_EXPORT_EXTRA}"
+XCODE_EXPORT_ARCHIVE_STATUS="$?"
+
+if [[ "$FLUTTER_BUILD_STATUS" != "0" ]] || [[ "$XCODE_CLEAN_ARCHIVE_STATUS" != "0" ]] || [[ "$XCODE_EXPORT_ARCHIVE_STATUS" != "0" ]]; then
+  BUILD_STATUS="99"
+  mark_status "failure" "iOS build failure"
 else
-    mark_status "failure" "iOS build failure"
+  BUILD_STATUS="0"
+  mark_status "success" "iOS build success"
 fi
 
 exit "$BUILD_STATUS"
