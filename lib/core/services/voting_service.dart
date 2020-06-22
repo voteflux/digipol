aa00c4c6a6e3a25bbac5c675387a20a345473a7b
Bazmati <barry@earsman.com>
2020-06-22 21:34:05 +1000

    Return txHash in BlillVoteSuccess

--- lib/core/services/voting_service.dart
+++ lib/core/services/voting_service.dart
@@ -70,36 +70,17 @@ class VotingService {
   Future<BillVoteSuccess> postVote(BillVote vote) async {
     //Box userBox = Hive.box("user_box");
     Box<BillVote> billVoteBox = Hive.box<BillVote>("bill_vote_box");
-    //final ethereumAddress = userBox.get('ethereumAddress') ?? null;
 
     // to delete
     final prefs = await SharedPreferences.getInstance();
     prefs.setString(vote.ballotId, vote.vote);
 
-    /*var body = json.encode(<String, dynamic>{
-      "pub_key": ethereumAddress,
-      "ballot_id": vote.ballotId,
-      "ballotspec_hash": vote.ballotSpecHash,
-      "constituency": vote.constituency,
-      "vote": vote.vote,
-    });*/
-
     var txHash = await submitVoteTransaction(vote.ballotSpecHash, vote.vote);
-
-    /*final http.Response response = await http.post(
-      'https://1j56c60pb0.execute-api.ap-southeast-2.amazonaws.com/dev/shitchain/',
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        "accept": "application/json",
-      },
-      body: body,
-    );*/
 
     if (txHash != "") {
       print("Transaction hash " + txHash);
       billVoteBox.add(BillVote(ballotId: vote.ballotId, vote: vote.vote));
-      //print(billVoteBox.getAt(0).ballotId.toString());
-      return BillVoteSuccess(ballotspecHash: vote.ballotSpecHash);
+      return BillVoteSuccess(ballotspecHash: txHash);
     } else {
       throw Exception('Failed cast vote');
     }
