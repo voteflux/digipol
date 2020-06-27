89eb63d70e2ab1a849d73f2a838832a4d7f131df
oscar <oscarmanderj@gmail.com>
2020-06-27 13:04:42 +1000

    clean up onboard, require onboarding when no user registered, add svgs

--- lib/ui/views/onboarding_view.dart
+++ lib/ui/views/onboarding_view.dart
@@ -1,5 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
+import 'package:voting_app/ui/views/login_view.dart';
 
 class OnBoardingView extends StatefulWidget {
   @override
@@ -14,7 +16,7 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   void initState() {
     super.initState();
     currentIndexPage = 0;
-    pageLength = 3;
+    pageLength = 6;
   }
 
   @override
@@ -24,58 +26,75 @@ class _OnBoardingViewState extends State<OnBoardingView> {
         MediaQuery.of(context).size.height - marginFromSafeArea;
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: heightOfScreen * 0.6,
-              child: PageView(
-                children: <Widget>[
-                  _buildWalkThrough(),
-                  _buildWalkThrough(),
-                  _buildWalkThrough()
-                ],
-                onPageChanged: (value) {
-                  setState(() => currentIndexPage = value.toDouble());
-                },
-              ),
-            ),
-            Container(
-              height: heightOfScreen * 0.1,
-              child: DotsIndicator(
-                dotsCount: pageLength,
-                position: currentIndexPage,
-                decorator: DotsDecorator(
-                  color: Colors.blue,
-                  activeColor: Colors.red,
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: Container(
+                  child: PageView(
+                    children: <Widget>[
+                      _buildWalkThrough(context, 'undraw_welcome_3gvl.svg',
+                          "Welcome to DigiPol! Where digital direct democracy starts."),
+                      _buildWalkThrough(context, 'undraw_process_e90d.svg',
+                          "DigiPol collates all the current bills in the Australian goverment for you."),
+                      _buildWalkThrough(context, 'undraw_voting_nvu7.svg',
+                          "So you can stay informed and vote directly on what matters most."),
+                      _buildWalkThrough(context, 'undraw_new_ideas_jdea.svg',
+                          "You can even vote on current issues created by the community or submit your own."),
+                      _buildWalkThrough(context, 'undraw_ethereum_fb7n.svg',
+                          "We don't store any of your information, and all votes are verified by the ethereum blockchain."),
+                      ProfilePage()
+                    ],
+                    onPageChanged: (value) {
+                      setState(() => currentIndexPage = value.toDouble());
+                    },
+                  ),
                 ),
               ),
-            ),
-            Container(height: heightOfScreen * 0.2)
-          ],
+              Container(
+                child: DotsIndicator(
+                  dotsCount: pageLength,
+                  position: currentIndexPage,
+                  decorator: DotsDecorator(
+                    color: Colors.grey,
+                    activeColor: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
 
-Widget _buildWalkThrough() {
+Widget _buildWalkThrough(context, graphic, text) {
   return Container(
     child: Stack(
       children: <Widget>[
         Container(
           child: Column(
             children: <Widget>[
-              Image.asset(
-                "assets/graphics/vote.png",
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: SvgPicture.asset('assets/graphics/' + graphic,
+                      semanticsLabel: 'Acme Logo'),
+                ),
               ),
               ListBody(
                 mainAxis: Axis.vertical,
                 children: <Widget>[
-                  Text(
-                    "StringConst.JOIN",
-                    textAlign: TextAlign.center,
-                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 40.0, right: 40.0, bottom: 60.0),
+                    child: Text(
+                      text,
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                  )
                 ],
               ),
             ],
