16b3937c7cb25bc0c64a041c9b1af03bcdd3d491
Max Kaye <m@xk.io>
2020-10-05 17:12:48 +1100

    use better directory in path_provider

--- lib/core/services/wallet.dart
+++ lib/core/services/wallet.dart
@@ -151,7 +151,15 @@ class WalletService {
 
   /// The file handle for the file used to store the wallet object.
   Future<File> walletFile() async {
-    var appDocsDir = await getApplicationDocumentsDirectory();
+    // regarding getApplicationDocumentsDirectory
+    // > Path to a directory where the application may place data that is user-generated, or that cannot otherwise be recreated by your application.
+    // > On iOS, this uses the NSDocumentDirectory API. Consider using getApplicationSupportDirectory instead if the data is not user-generated.
+    //var appDocsDir = await getApplicationDocumentsDirectory();
+
+    // regarding getApplicationSupportDirectory
+    // > Use this for files you don’t want exposed to the user. Your app should not use this directory for user data files.
+    var appDocsDir = await getApplicationSupportDirectory();
+
     String wp = _walletDirectoryPath.getOrElse(() => appDocsDir.path);
     return File('${wp}/$WALLET_FILE_NAME');
   }
