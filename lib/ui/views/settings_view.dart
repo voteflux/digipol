ed7e99a98338b601a3f084652f3b55d5d7daa619
kipling <kip.crossing@gmail.com>
2020-11-04 22:23:47 +1100

    new version 0.1.5

--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -106,7 +106,7 @@ class _SettingsPageState extends State<SettingsPage> {
                       style: Theme.of(context).textTheme.headline5,
                     ),
                     Text(
-                      "v0.1.3",
+                      "v0.1.5",
                       style: Theme.of(context).textTheme.headline6,
                     ),
                     Divider(
