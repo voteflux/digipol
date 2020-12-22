297daa6eb396063843d2c588fabc064f6234bc83
Kasina Kasinrerk <kkasinrerk@student.unimelb.edu.au>
2020-12-22 17:51:51 +1100

    refactor OnBoardingView

--- lib/ui/views/onboarding_view.dart
+++ lib/ui/views/onboarding_view.dart
@@ -2,7 +2,7 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:voting_app/ui/views/login_view.dart';
+import 'package:voting_app/ui/widgets/in_line_icon.dart';
 
 class OnBoardingView extends StatefulWidget {
   @override
@@ -14,39 +14,40 @@ class OnboardingSlide {
   String heading;
   String body;
   String label;
-
-  OnboardingSlide(this.image, this.heading, this.body, this.label);
+  OnboardingSlide(this.image, this.label, this.heading, this.body);
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+
   double currentIndexPage = 0;
   /*late*/ int pageLength;
+
   List<OnboardingSlide> pages = [
     OnboardingSlide(
         'undraw_Hello_qnas.svg',
-        'Hi!',
-        'Welcome to DigiPol! Where digital direct democracy starts. (draft)',
-        'Welcome!'),
+        'Welcome to DigiPol!',
+        'Ready to make a change?',
+        'The voting app where you can have your say!'),
     OnboardingSlide(
         'undraw_process_e90d.svg',
-        'What is DigiPol?',
-        'DigiPol collates all the current bills in the Australian government for you. So you can read about them and express your support or opposition by voting directly on a Bill. (draft)',
-        'The DigiPol Process'),
+        'Welcome to DigiPol!',
+        'Send a message directly to politicians.',
+        'By voting directly on the bills that affect you and your fellow citizens, DigiPol, gives you the power to have your voice heard.'),
     OnboardingSlide(
         'undraw_voting_nvu7.svg',
-        'How does it do it?',
-        'So you can stay informed & make your opinion known when it matters most. (draft)',
-        'DigiPol keeps you informed'),
+        'Welcome to DigiPol!',
+        'Your decisions are secure.',
+        'By using state-of-the-art technology, no one can alter your votes but you.\nFind out more in `icon-ios-android.png`Settings.'),
     OnboardingSlide(
         'undraw_new_ideas_jdea.svg',
-        'Issues',
-        'You can even vote on current issues created by the community. (draft)',
-        "DigiPol let's you vote on issues."),
+        'Welcome to DigiPol!',
+        'Hear from your community.',
+        "Coming soon - You can also comment on bills and see what others have to say about them!"),
     OnboardingSlide(
         'undraw_ethereum_fb7n.svg',
-        'Zero',
-        'None of your personal information is stored, and all votes are verified by our blockchain. (draft)',
-        'Your personal info is safe and all votes are verified by a blockchain.'),
+        'Welcome to DigiPol!',
+        'Have your say on what\'s important to you.',
+        'We\'ve made it easier to find the bills and issues that you want to vote on. Start by selecting some tags below, and you can use the `icon-ios-android.png`(Interest) button to customise your result in the Bill Hub at any time.'),
   ];
 
   @override
@@ -97,18 +98,22 @@ class _OnBoardingViewState extends State<OnBoardingView> {
 }
 
 Widget _buildWalkThrough(BuildContext context, String graphic, String heading,
-    String text, String label) {
+    String body, String label) {
+  String logo = 'assets/graphics/static-logo.png';
   return Container(
     child: Stack(
       children: <Widget>[
         Container(
           child: Column(
             children: <Widget>[
+              Spacer(
+                flex: 1
+              ),
               Flexible(
+                flex: 2,
                 child: Padding(
                   padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: SvgPicture.asset('assets/graphics/' + graphic,
-                      semanticsLabel: label),
+                  child: Image.asset(logo, height: 70.0, width: 70.0),
                 ),
               ),
               ListBody(
@@ -116,24 +121,41 @@ Widget _buildWalkThrough(BuildContext context, String graphic, String heading,
                 children: <Widget>[
                   Padding(
                     padding:
-                        EdgeInsets.only(left: 40.0, right: 40.0, bottom: 20.0),
+                    EdgeInsets.only(
+                        left: 40.0,
+                        right: 40.0,
+                        top: 40,
+                        bottom: 20.0
+                    ),
                     child: Text(
-                      heading,
-                      style: Theme.of(context).textTheme.headline4,
+                      label,
+                      style: Theme.of(context).textTheme.headline5,
                       textAlign: TextAlign.center,
                     ),
                   ),
                   Padding(
                     padding:
-                        EdgeInsets.only(left: 40.0, right: 40.0, bottom: 60.0),
+                    EdgeInsets.only(left: 40.0, right: 40.0, bottom: 20.0),
                     child: Text(
-                      text,
-                      style: Theme.of(context).textTheme.bodyText1,
+                      heading,
+                      style: Theme.of(context).textTheme.headline4,
                       textAlign: TextAlign.center,
                     ),
-                  )
+                  ),Padding(
+                    padding:
+                    EdgeInsets.only(left: 40.0, right: 40.0, bottom: 20.0),
+                    child: InLineIcon(text: body)
+                  ),
                 ],
               ),
+              Flexible(
+                flex: 10,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 80.0, right: 80.0),
+                  child: SvgPicture.asset('assets/graphics/' + graphic,
+                      semanticsLabel: label),
+                  ),
+              )
             ],
           ),
         ),
@@ -141,3 +163,6 @@ Widget _buildWalkThrough(BuildContext context, String graphic, String heading,
     ),
   );
 }
+
+
+
