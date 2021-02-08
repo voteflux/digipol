74ba9a4e64b629aa47f6ed3afd16a77b4614a62a
kipling <kip.crossing@gmail.com>
2021-02-08 11:07:04 +1100

    Abstract username draw contents to UsernameUpdater widget

--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -23,8 +23,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  final TextEditingController _userName = TextEditingController();
-  final TextEditingController _pinCode = TextEditingController();
   final TextEditingController _oldPinCode = TextEditingController();
   final TextEditingController _newPinCode = TextEditingController();
   final TextEditingController _confirmNewPin = TextEditingController();
@@ -140,124 +138,7 @@ class _SettingsPageState extends State<SettingsPage> {
           setState(() {
             drawer = Drawer(
               key: UniqueKey(),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                color: Theme.of(context).cardTheme.color,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Stack(
-                        alignment: Alignment.centerLeft,
-                        children: [
-                          IconButton(
-                            color: Theme.of(context).colorScheme.primary,
-                            icon: Icon(Icons.close),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Text(
-                                "Profile",
-                                style: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      CircleAvatar(
-                        radius: 45,
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                        child: Icon(Icons.person,
-                            size: 60, color: Theme.of(context).backgroundColor),
-                      ),
-                      SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Username",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      TextField(
-                          controller: _userName,
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Theme.of(context).cardTheme.color,
-                              border: OutlineInputBorder(),
-                              hintStyle: Theme.of(context).textTheme.bodyText1,
-                              hintText: 'New username')),
-                      SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Your pin",
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      PinCodeTextField(
-                          controller: _pinCode,
-                          pinBoxWidth: 45,
-                          pinBoxHeight: 50,
-                          pinTextStyle: TextStyle(
-                              fontSize: 18,
-                              color: Theme.of(context).colorScheme.primary),
-                          onDone: (text) {
-                            print(text);
-                          },
-                          highlight: true,
-                          highlightColor: Theme.of(context).colorScheme.primary,
-                          //highlightPinBoxColor:
-                          //Theme.of(context).colorScheme.onSurface,
-                          maxLength: 4,
-                          hasUnderline: false,
-                          pinBoxColor: Theme.of(context).cardTheme.color,
-                          defaultBorderColor:
-                              Theme.of(context).colorScheme.onSurface,
-                          pinBoxRadius: 10.0,
-                          hasTextBorderColor:
-                              Theme.of(context).colorScheme.primary),
-                      SizedBox(height: 30),
-                      RaisedButton(
-                        child: Text("Update"),
-                        onPressed: () {
-                          if (_pinCode.text == userBox.get('pincode')) {
-                            userBox.put('firstName', _userName.text);
-                            final snackBar = SnackBar(
-                              content: Text('User name has been changed'),
-                            );
-                            Navigator.of(context).pop();
-                            Future.delayed(Duration(milliseconds: 300), () {
-                              Scaffold.of(context).showSnackBar(snackBar);
-                            });
-                          }
-                          ;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              child: UsernameUpdater(),
             );
           });
           //_scaffoldKey.currentState.openEndDrawer();
@@ -625,5 +506,151 @@ class _SettingsPageState extends State<SettingsPage> {
                 Clipboard.setData(ClipboardData(text: pubKey));
               })
         ]);
+  }
+}
+
+class UsernameUpdater extends StatefulWidget {
+  @override
+  _UsernameUpdaterState createState() => _UsernameUpdaterState();
+}
+
+class _UsernameUpdaterState extends State<UsernameUpdater> {
+  final TextEditingController _userName = TextEditingController();
+  final TextEditingController _pinCode = TextEditingController();
+  Box userBox = Hive.box<String>(HIVE_USER_PREFS_STR);
+  bool hasError = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      color: Theme.of(context).cardTheme.color,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.centerLeft,
+              children: [
+                IconButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      "Profile",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            CircleAvatar(
+              radius: 45,
+              backgroundColor:
+              Theme.of(context).colorScheme.secondary,
+              child: Icon(Icons.person,
+                  size: 60, color: Theme.of(context).backgroundColor),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Username",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+                controller: _userName,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Theme.of(context).cardTheme.color,
+                    border: OutlineInputBorder(),
+                    hintStyle: Theme.of(context).textTheme.bodyText1,
+                    hintText: 'New username')),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Your pin",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            PinCodeTextField(
+                controller: _pinCode,
+                pinBoxWidth: 45,
+                pinBoxHeight: 50,
+                pinTextStyle: TextStyle(
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.primary),
+                onDone: (text) {
+                  print(text);
+                },
+                highlight: true,
+                highlightColor: Theme.of(context).colorScheme.primary,
+                //highlightPinBoxColor:
+                //Theme.of(context).colorScheme.onSurface,
+                maxLength: 4,
+                hasUnderline: false,
+                hasError: hasError,
+                pinBoxColor: Theme.of(context).cardTheme.color,
+                defaultBorderColor:
+                Theme.of(context).colorScheme.onSurface,
+                pinBoxRadius: 10.0,
+                hasTextBorderColor:
+                Theme.of(context).colorScheme.primary),
+
+            Visibility(
+              child: Text("Wrong PIN!",
+              ),
+              visible: hasError,
+            ),
+            SizedBox(height: 30),
+            RaisedButton(
+              child: Text("Update"),
+              onPressed: () {
+                if (_pinCode.text == userBox.get('pincode')) {
+                  userBox.put('firstName', _userName.text);
+                  final snackBar = SnackBar(
+                    content: Text('User name has been changed'),
+                  );
+                  Navigator.of(context).pop();
+                  Future.delayed(Duration(milliseconds: 300), () {
+                    Scaffold.of(context).showSnackBar(snackBar);
+                  });
+                }else{
+                  hasError = true;
+                  setState(() {
+
+                  });
+                }
+                ;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
